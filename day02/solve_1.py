600b55ec2a8a9ef0c319b63d82c24178d8c69be3
Richard Panman <rpanman@sonatype.com>
2022-12-02 08:59:13 +0000

    Day 2

---
+++ day02/solve_1.py
@@ -0,0 +1,26 @@
+
+def solve()->int:
+    total = 0
+    # A for Rock, B for Paper, and C for Scissors
+    # X for Rock, Y for Paper, and Z for Scissors
+    # 0 if you lost, 3 if the round was a draw, and 6 if you won
+    # shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
+    score_map = {
+        "A X": 3+1,
+        "A Y": 6+2,
+        "A Z": 0+3,
+        "B X": 0+1,
+        "B Y": 3+2,
+        "B Z": 6+3,
+        "C X": 6+1,
+        "C Y": 0+2,
+        "C Z": 3+3,
+    }
+    
+    with open("day02/puzzle_input.txt", "r", encoding="utf-8") as input_file:
+        while line := input_file.readline():
+            print(line.strip())
+            total+=int(score_map[line.strip()])
+    return total
+if __name__ == "__main__":
+    print(solve())
---
+++ day02/solve_2.py
@@ -0,0 +1,42 @@
+
+def solve()->int:
+    total = 0
+    # A for Rock, B for Paper, and C for Scissors
+    # X for Rock, Y for Paper, and Z for Scissors
+    # 0 if you lost, 3 if the round was a draw, and 6 if you won
+    # shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
+    
+    # X to lose
+    # Y to draw
+    # X to win
+    strategy_map = {
+        "A X": "A C",
+        "A Y": "A A",
+        "A Z": "A B",
+        "B X": "B A",
+        "B Y": "B B",
+        "B Z": "B C",
+        "C X": "C B",
+        "C Y": "C C",
+        "C Z": "C A",
+    }
+    
+    score_map = {
+        "A A": 3+1,
+        "A B": 6+2,
+        "A C": 0+3,
+        "B A": 0+1,
+        "B B": 3+2,
+        "B C": 6+3,
+        "C A": 6+1,
+        "C B": 0+2,
+        "C C": 3+3,
+    }
+    
+    with open("day02/puzzle_input.txt", "r", encoding="utf-8") as input_file:
+        while line := input_file.readline():
+            print(line.strip())
+            total+=int(score_map[strategy_map[line.strip()]])
+    return total
+if __name__ == "__main__":
+    print(solve())
