13ad88758a289bd7b915002439addf5a2a5e6d03
Richard Panman <rpanman@sonatype.com>
2022-12-03 09:18:24 +0000

    Day 3

--- day03/solve_2.py
+++ day03/solve_2.py
@@ -1,18 +1,18 @@
-def get_matching_letter(str1: list[str], str2: list[str]) -> str:
-    return list(set(str1) & set(str2))[0]
+"""Advent of Code 2022 - day 3 - problem 2"""
 
-
-priorities = {chr(code): code - 96 for code in range(97, 123)}
+priority_score_for_item = {chr(code): code - 96 for code in range(97, 123)}
 for code in range(65, 91):
-    priorities[chr(code)] = code - 64 + 26
+    priority_score_for_item[chr(code)] = code - 64 + 26
 
 
-def elf_team_rucksack_distributor(rucksacks: list, elf_team_size: int) -> list:
+def elf_team_rucksack_distributor(*, rucksacks: list, elf_team_size: int) -> list:
+    """Distribute the rucksacks across teams of the given size"""
     for start_rucksack in range(0, len(rucksacks), elf_team_size):
         yield rucksacks[start_rucksack : start_rucksack + elf_team_size]
 
 
-def find_common_item_in_elf_team_rucksacks(rucksacks: list) -> str:
+def find_common_item_in_elf_team_rucksacks(*, rucksacks: list) -> str:
+    """Find the common item in all the rucksacks"""
     the_set = {*rucksacks[0]}
     for rucksack in rucksacks[1:]:
         the_set = the_set & {*rucksack}
@@ -20,12 +20,17 @@ def find_common_item_in_elf_team_rucksacks(rucksacks: list) -> str:
 
 
 def solve() -> int:
+    """Solve the puzzle"""
     with open("day03/puzzle_input.txt", "r", encoding="utf-8") as rucksack:
         all_rucksacks = rucksack.read().splitlines()
     sum_of_priorities = 0
-    for elf_team_rucksacks in elf_team_rucksack_distributor(all_rucksacks, 3):
-        common_item = find_common_item_in_elf_team_rucksacks(elf_team_rucksacks)
-        sum_of_priorities += priorities[common_item]
+    for elf_team_rucksacks in elf_team_rucksack_distributor(
+        rucksacks=all_rucksacks, elf_team_size=3
+    ):
+        common_item = find_common_item_in_elf_team_rucksacks(
+            rucksacks=elf_team_rucksacks
+        )
+        sum_of_priorities += priority_score_for_item[common_item]
     return sum_of_priorities
 
 
