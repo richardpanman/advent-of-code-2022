2256e71dc7dea697e2bc375b696481790a40ce23
Richard Panman <rpanman@sonatype.com>
2022-12-05 11:49:40 +0000

    Made the code more readable

--- day05/combined_solve.py
+++ day05/combined_solve.py
@@ -1,25 +1,26 @@
 import re
 
 
-def get_stack_and_instruction_lines():
-    with open("day05/puzzle_input.txt", "r", encoding="utf-8") as the_file:
-        stack_lines = []
-        instruction_lines = []
-        READING_STACK = True
+def get_stack_and_instruction_lines(filename: str) -> tuple[list[str], list[str]]:
+    with open(filename, "r", encoding="utf-8") as the_file:
+        return_stack_lines = []
+        return_instruction_lines = []
+        reading_the_stack = True
+
         all_lines = the_file.readlines()
         for line in all_lines:
             line = line.replace("\n", "")
             if line == "":
-                READING_STACK = False
+                reading_the_stack = False
                 continue
-            if READING_STACK:
-                stack_lines.append(line)
+            if reading_the_stack:
+                return_stack_lines.append(line)
                 continue
-            instruction_lines.append(line)
-    return stack_lines, instruction_lines
+            return_instruction_lines.append(line)
+    return return_stack_lines, return_instruction_lines
 
 
-def convert_stack_lines_to_stacks(input_stack_lines):
+def convert_stack_lines_to_stacks(input_stack_lines: list[str]) -> list[list[str]]:
     input_stack_lines = input_stack_lines[:-1]
     input_stack_lines = [re.findall(r".(.).\s?", stack) for stack in input_stack_lines]
     input_stack_lines.reverse()  # get the right stack on the bottom
@@ -29,20 +30,84 @@ def convert_stack_lines_to_stacks(input_stack_lines):
     return return_stacks
 
 
-def move(our_stacks, our_instructions):
+def move_crates_one_at_a_time(
+    our_stacks: list[list[str]], our_instructions: list[str]
+) -> None:
     for instruction in our_instructions:
-        items, from_stack, to_stack = re.findall(
-            r"move (\d+) from (\d+) to (\d+)", instruction
-        )[0]
+        items, from_stack, to_stack = interpret_instructions(instruction)
         for _x in range(int(items)):
-            our_stacks[int(to_stack) - 1].append(our_stacks[int(from_stack) - 1].pop())
+            crate_that_is_moving = pick_up_single_crate(our_stacks, from_stack)
+            put_crate_on_new_stack(our_stacks, to_stack, crate_that_is_moving)
 
 
-def answer(our_crates):
+def put_crate_on_new_stack(our_stacks, to_stack, crate_that_is_moving):
+    our_stacks[int(to_stack) - 1].append(crate_that_is_moving)
+
+
+def pick_up_single_crate(our_stacks, from_stack):
+    return our_stacks[int(from_stack) - 1].pop()
+
+
+def move_multiple_crates(
+    our_stacks: list[list[str]], our_instructions: list[str]
+) -> None:
+    for instruction in our_instructions:
+        items, from_stack, to_stack = interpret_instructions(instruction)
+        crates_that_are_moving = select_crates_to_move(our_stacks, items, from_stack)
+        pick_up_the_crates(our_stacks, items, from_stack)
+        put_crates_on_new_stack(our_stacks, to_stack, crates_that_are_moving)
+
+
+def put_crates_on_new_stack(our_stacks, to_stack, crates_that_are_moving):
+    our_stacks[int(to_stack) - 1] += crates_that_are_moving
+
+
+def pick_up_the_crates(our_stacks, items, from_stack):
+    our_stacks[int(from_stack) - 1] = our_stacks[int(from_stack) - 1][
+        : len(our_stacks[int(from_stack) - 1]) - int(items)
+    ]
+
+
+def select_crates_to_move(our_stacks, items, from_stack):
+    return our_stacks[int(from_stack) - 1][
+        len(our_stacks[int(from_stack) - 1]) - int(items) :
+    ]
+
+
+def interpret_instructions(instruction):
+    items, from_stack, to_stack = re.findall(
+        r"move (\d+) from (\d+) to (\d+)", instruction
+    )[0]
+
+    return items, from_stack, to_stack
+
+
+def get_crates_on_top_of_stacks(our_crates: list[list[str]]) -> str:
     return "".join([entry[-1] for entry in our_crates])
 
 
-stack_lines, instruction_lines = get_stack_and_instruction_lines()
-stacks = convert_stack_lines_to_stacks(stack_lines)
-move(stacks, instruction_lines)
-print(answer(stacks))
+def get_answer_for_file(filename: str, moving_function: callable) -> str:
+    stack_lines, instruction_lines = get_stack_and_instruction_lines(filename=filename)
+    stacks = convert_stack_lines_to_stacks(stack_lines)
+    moving_function(stacks, instruction_lines)
+    return get_crates_on_top_of_stacks(stacks)
+
+
+assert get_answer_for_file("day05/test_input.txt", move_crates_one_at_a_time) == "CMZ"
+assert (
+    get_answer_for_file("day05/puzzle_input.txt", move_crates_one_at_a_time)
+    == "TWSGQHNHL"
+)
+
+assert (
+    get_answer_for_file(
+        filename="day05/test_input.txt", moving_function=move_multiple_crates
+    )
+    == "MCD"
+)
+assert (
+    get_answer_for_file(
+        filename="day05/puzzle_input.txt", moving_function=move_multiple_crates
+    )
+    == "JNRSCDWPP"
+)
